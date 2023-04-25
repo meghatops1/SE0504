aa007fdd1482cf2790326e42ff503a09870d1afd
Megha <meghapatel.tops@gmail.com>
2023-04-25 09:51:01 +0530

    c start

---
+++ New folder/datatype.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main(){
+	char ch='a';
+	const int  a=100;
+	float pi=3.14;
+	printf("\n char =%c",ch);
+	printf("\n integer=%d",a);
+	printf("\n pi=%f",pi);
+}
---
+++ New folder/hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>//std input ouput header file
+//#include<conio.h>
+//single line comments
+/*
+multi line comments
+
+*/
+main(){
+	printf("Hello wolrd");
+	printf("\nTops \t technology");//\n for new line \t for put tab space 
+}
